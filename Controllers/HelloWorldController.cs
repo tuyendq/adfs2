ac8c329f49983894a257cb577fbd039f634f9f54
Tuyen Dang <tuyendq@gmail.com>
2020-04-22 13:37:45 +0700

    add view: Index to HelloWorld

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -5,6 +5,10 @@ namespace adfs2.Controllers
 {
     public class HelloWorldController : Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
         // GET: /HelloWorld/Welcome/
         public string Welcome(string name, int numTimes = 1)
         {
